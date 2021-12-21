119900e9f53c5246a210cc1b6921e5c393af8be9
ghost0r10n <rionhacker@gmail.com>
2021-12-21 03:58:32 +0100

    Completed metrics, and result check, started memory mon

--- Emma.py
+++ Emma.py
@@ -1,6 +1,6 @@
 import time
 from functools import wraps
-
+import psutil
 
 
 class Emma(object):
@@ -8,35 +8,68 @@ class Emma(object):
 
 
 
-    def timer():
-        #TODO This method will show the time of execution of the function
-        pass
-
     def result_should_be(self, result:any, should_be:any)->any:
         return result==should_be
+
+    
+    def metrics_function(self, function, args, kwargs):
+        start = time.perf_counter()
+        function(*args, **kwargs)
+        end = time.perf_counter()
+        return f"{end - start:0.10f}s"
+
 
     def metrics(self, function):
         @wraps(function)
         def wrapper(*args, **kwargs):
-            start = time.perf_counter()
-            function(*args, **kwargs)
-            end = time.perf_counter()
-            print(f"Execution time --> {end - start:0.4f}")
+            elapsed_time = self.metrics_function(function)
+            print(f"Execution time --> {elapsed_time}")
         return wrapper
 
     def unit_test(self, **kwargs):
+        # Variables pre instansiated
+        should_be = None
+        metrics = None
+        mem_usage = None
         if 'should_be' in kwargs:
             should_be = kwargs['should_be']
-        if 'metrics' in kwargs:
+        if 'metrics' in kwargs and type(kwargs['metrics']) == bool:
             metrics = kwargs['metrics']
+        if 'profile_mem' in kwargs and type(kwargs['profile_mem'])==bool:
+            mem_usage = kwargs['profile_mem']
+
+
         def unit_test_function(function):
             @wraps(function)
             def wrapper(*args, **kwargs):
-                print(f'RUNNING TEST ON --> {function.__name__}')
+                print(f'RUNNING TEST ON --> {function.__name__}\n')
                 result = function(*args, **kwargs)
-                if not self.result_should_be(result, should_be):
+                if should_be!=None and not self.result_should_be(result, should_be):
                     print("FAILED --> the result of the function is incorrect")
-                self.metrics(function)
+                    print(f"    SHOULD BE :  {should_be}")
+                    print(f"    BUT IT WAS:  {result}\n") 
+                
+                if metrics:
+                    elapsed_time = self.metrics_function(function, args, kwargs)
+                    print(f"Execution time --> {elapsed_time}")
+                
+                if mem_usage:
+                    usage_before = psutil.virtual_memory()
+                    function(*args, **kwargs)
+                    usage_after = psutil.virtual_memory()
+
+                    usage_before_rounded = round(usage_before.available/1024)
+                    usage_after_rounded = round(usage_after.available/1024)
+                    print(usage_before_rounded)
+                    print(usage_after_rounded)
+                    print(f'DIFFERENCE AFTER RUNNING --> {usage_before_rounded-usage_after_rounded} KB')
+
                 return function
             return wrapper
         return unit_test_function
+
+
+
+
+
+
--- debug.py
+++ debug.py
@@ -4,11 +4,11 @@ from Emma import Emma
 emma = Emma()
 
 
-@emma.unit_test(should_be=0, metrics=True)
+@emma.unit_test(profile_mem=True)
 def tester_function(length:int):
     x =1 
-    #for i in range(length):
-    #    x += i
+    for i in range(length):
+        x += i
     return x
 
 if __name__ == '__main__':    
